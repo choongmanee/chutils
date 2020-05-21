1228af44f32053996f035c0c9089aabd44fcf3de
choongmanee <choongmanee@users.noreply.github.com>
2020-05-21 06:49:19 +0000

    Automated deployment: Thu May 21 06:49:19 UTC 2020 474a33b984332cda59755cdf18c569b7142e9012

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,18 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "e0ab4cc0badf44fc133a555b3658092e",
-"/": "e0ab4cc0badf44fc133a555b3658092e",
-"main.dart.js": "62ecf8998db830630903b226914998e0",
-"manifest.json": "0e4e86649f166121fe0f80a2987ecebf",
+  "manifest.json": "0e4e86649f166121fe0f80a2987ecebf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "9f8604b61d5f12bdcaf6abd65be0a8d9",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9"
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"index.html": "e0ab4cc0badf44fc133a555b3658092e",
+"/": "e0ab4cc0badf44fc133a555b3658092e",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"main.dart.js": "62ecf8998db830630903b226914998e0"
 };
 
 self.addEventListener('activate', function (event) {
